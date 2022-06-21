9a5778364ea1fc60616e4467947ce403917a75ae
Matthieu Dartiguenave <matthieu.dartiguenave@gmail.com>
2022-06-21 16:43:49 +0200

    Capture output selection

--- src/debug.rs
+++ src/debug.rs
@@ -1,5 +1,7 @@
-use gloo::{console::info, events::EventListener};
+use gloo::events::EventListener;
+use log::info;
 use pulldown_cmark::{html::push_html, Parser};
+
 use wasm_bindgen::JsCast;
 
 use web_sys::{HtmlElement, HtmlTextAreaElement};
@@ -9,11 +11,14 @@ pub struct EditorDebug {
     output_ref: NodeRef,
     input_ref: NodeRef,
     selection_listener: Option<EventListener>,
+    selection_input: Option<(u32, u32)>,
+    selection_output: Option<(u32, u32)>,
 }
 
 pub enum EditorDebugEvent {
     ContentChanged(String),
-    SelectionChanged((u32, u32)),
+    InputSelectionChanged((u32, u32)),
+    OutputSelectionChanged((u32, u32)),
 }
 
 impl Component for EditorDebug {
@@ -25,6 +30,8 @@ impl Component for EditorDebug {
             output_ref: NodeRef::default(),
             input_ref: NodeRef::default(),
             selection_listener: None,
+            selection_input: None,
+            selection_output: None,
         }
     }
 
@@ -33,13 +40,43 @@ impl Component for EditorDebug {
             let document = gloo::utils::document();
 
             let link = ctx.link().clone();
-            let input = self.input_ref.cast::<HtmlTextAreaElement>().unwrap();
+            let input = self.input_ref.cast::<HtmlElement>().unwrap();
+            let input_textarea = self.input_ref.cast::<HtmlTextAreaElement>().unwrap();
 
-            let listener = EventListener::new(&document, "selectionchange", move |e| {
-                if let (Ok(Some(start)), Ok(Some(end))) =
-                    (input.selection_start(), input.selection_end())
-                {
-                    link.send_message(EditorDebugEvent::SelectionChanged((start, end)));
+            let output = self.output_ref.cast::<HtmlElement>().unwrap();
+
+            let listener = EventListener::new(&document.clone(), "selectionchange", move |e| {
+                let element = document
+                    .active_element()
+                    .map(|e| e.dyn_into::<HtmlElement>());
+
+                if let Some(Ok(element)) = element {
+                    if output == element {
+                        let selection = gloo::utils::window().get_selection().unwrap().unwrap();
+
+                        if selection.range_count() > 0 {
+                            if let Ok(range) = selection.get_range_at(0) {
+                                if let (Ok(start), Ok(end)) =
+                                    (range.start_offset(), range.end_offset())
+                                {
+                                    link.send_message(EditorDebugEvent::OutputSelectionChanged((
+                                        start, end,
+                                    )));
+                                }
+                            }
+                        };
+                    }
+
+                    if input == element {
+                        if let (Ok(Some(start)), Ok(Some(end))) = (
+                            input_textarea.selection_start(),
+                            input_textarea.selection_end(),
+                        ) {
+                            link.send_message(EditorDebugEvent::InputSelectionChanged((
+                                start, end,
+                            )));
+                        };
+                    }
                 }
             });
 
@@ -60,9 +97,13 @@ impl Component for EditorDebug {
 
                 true
             }
-            EditorDebugEvent::SelectionChanged((start, end)) => {
-                info!("Selection: {} - {}", start, end);
-                false
+            EditorDebugEvent::InputSelectionChanged((start, end)) => {
+                self.selection_input = Some((start, end));
+                true
+            }
+            EditorDebugEvent::OutputSelectionChanged((start, end)) => {
+                self.selection_output = Some((start, end));
+                true
             }
         }
     }
@@ -77,10 +118,27 @@ impl Component for EditorDebug {
         });
 
         html! {
-               <div style="display: flex; height: 100%">
+            <div style="display: flex; height: 100%; flex-direction: column">
+               <div style="display: flex; flex: 2; height: 100%">
                     <textarea style="flex: 1" oninput={content_changed}  ref={self.input_ref.clone()}/>
-                    <div style="flex: 1;" ref={self.output_ref.clone()}></div>
+                    <div style="flex: 1;" tabindex="0" ref={self.output_ref.clone()}></div>
                </div>
+               <div style="flex: 1">
+               if let Some((start, end)) = self.selection_input
+               {
+                     <div>
+                          {"Input: "} <span>{start}</span> {" - "} <span>{end}</span>
+                     </div>
+               }
+
+                if let Some((start, end)) = self.selection_output
+               {
+                     <div>
+                          {"Output: "} <span>{start}</span> {" - "} <span>{end}</span>
+                     </div>
+               }
+               </div>
+            </div>
         }
     }
 }
